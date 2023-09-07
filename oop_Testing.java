f5a0cf7ebf96c1b5dd34e7e4e6d766bd269dd427
Kura <surya.dharmasaputra@ui.ac.id>
2023-09-07 19:10:06 +0700

    tambahan variabel untuk test

--- oop_Testing.java
+++ oop_Testing.java
@@ -7,6 +7,7 @@ public class oop_Testing{
             
             int a = Integer.parseInt(scan.readLine());
             int b = Integer.parseInt(scan.readLine());
+            int c = 3;
 
             System.out.println("Hasil Penjumlahan = "+(a + b));
      }
